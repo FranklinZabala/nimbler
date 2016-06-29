0ae009b0cb60e45092811bc72932d0e9e992941e
Franklin Zabala <research@zabala.com>
2016-06-28 22:54:48 -0400

    added knockout, browserify and require.js multiple components binding proof of concept

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var jQuery = require("jquery");
+var ko = require("knockout");
+var host = location.origin.replace(/^http/, "ws");
+var ws = new WebSocket(host);
+
+jQuery(document).ready(function () {
+    ws.onmessage = function (e) {        
+        ws.send("ping back " + e);
+    };
+
+    ko.components.register("panel", {
+        viewModel: function (params) {
+            var self = this;
+            self.label = params.label;
+        },
+        template: "<div class='panel' data-bind='text: label'></div>"
+    });
+    
+    ko.components.register("textbox", {
+        viewModel: function (params) {
+            var self = this;
+            self.text = params.text;
+        },
+        template: "<div class='textbox'><input type='text' data-bind='textInput: text'></div>"
+    });    
+    
+    ko.applyBindings({
+        text: ko.observable("fasdf"),
+    });
+});
